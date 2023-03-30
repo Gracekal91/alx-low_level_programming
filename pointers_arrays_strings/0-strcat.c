4aeecec5135dc7fe172a8adbca49b301aa0fc2c9
Grace Kalombo <kalombograce1@gmail.com>
2023-03-29 21:13:58 -0700

    strcat

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+/*
+ * File: 0-strcat.c
+ * Auth: Brennan D Baraban
+ */
+
+#include "holberton.h"
+
+
+char *strcat(char *dest, const char *src)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+	return (dest);
+}
