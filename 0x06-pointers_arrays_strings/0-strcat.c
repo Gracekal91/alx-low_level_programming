d54b07f5e106cca008406588a8d1ee6f902e81eb
Grace Kalombo <kalombograce1@gmail.com>
2023-03-29 21:37:09 -0700

    concat strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,33 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 
-char *concat_strings(const char *s1, const char *s2) {
-    size_t s1_len = strlen(s1);
-    size_t s2_len = strlen(s2);
-    char *result = (char *)malloc(s1_len + s2_len + 1);
+char *_strcat(char *dest, const char *src);
 
-    if (result == NULL) {
-        fprintf(stderr, "Error: Failed to allocate memory for concatenated string.\n");
-        return NULL;
-    }
+int main(void)
+{
+    char dest[20] = "Hello";
+    const char *src = " world!";
 
-    strcpy(result, s1);
-    strcat(result, s2);
+    printf("Before concatenation: %s\n", dest);
 
-    return result;
-}
+    _strcat(dest, src);
 
-int main() {
-    const char *str1 = "Hello, ";
-    const char *str2 = "world!";
-
-    char *result = concat_strings(str1, str2);
-    if (result != NULL) {
-        printf("%s\n", result);
-        free(result);
-    }
+    printf("After concatenation: %s\n", dest);
 
     return 0;
 }
+
 
