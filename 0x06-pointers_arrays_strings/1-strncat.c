e6286a816d92b75f978ac82d80631171676f2259
Grace Kalombo <kalombograce1@gmail.com>
2023-03-29 21:58:27 -0700

    fixes

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -7,6 +7,14 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ * Prototype: char *_strncat(char *dest, char *src, int n);
+ * The _strncat function is similar to the _strcat function, except that
+ * it will use at most n bytes from src; and
+ * src does not need to be null-terminated if it contains n or more bytes
+ * Return a pointer to the resulting string dest
+*/
+
 char *_strncat(char *dest, char *src, int n)
 {
 	int index = 0, dest_len = 0;
