d8cadecd9d9211082f079840332bd30bb0f0ae3d
Grace Kalombo <kalombograce1@gmail.com>
2023-03-30 17:42:03 -0700

    hot fix

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -18,69 +18,53 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r);
  * Return: If r can store the sum - a pointer to the result.
  *         If r cannot store the sum - 0.
  */
-char *add_strings(char *n1, char *n2, char *r, int r_index)
-{
-	int num, tens = 0;
+#include <stdio.h>
+#include <string.h>
 
-	for (; *n1 && *n2; n1--, n2--, r_index--)
-	{
-		num = (*n1 - '0') + (*n2 - '0');
-		num += tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
-	}
+char *infinite_add(char *n1, char *n2, char *r, int size_r) {
+    int len1 = strlen(n1);
+    int len2 = strlen(n2);
+    int carry = 0;
+    int i, j;
 
-	for (; *n1; n1--, r_index--)
-	{
-		num = (*n1 - '0') + tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
-	}
+    // check if result can fit in buffer
+    if (len1 + len2 > size_r - 1) {
+        return 0;
+    }
 
-	for (; *n2; n2--, r_index--)
-	{
-		num = (*n2 - '0') + tens;
-		*(r + r_index) = (num % 10) + '0';
-		tens = num / 10;
-	}
+    // add digits from right to left, starting from least significant digit
+    for (i = len1 - 1, j = len2 - 1; i >= 0 || j >= 0; i--, j--) {
+        int sum = carry;
+        if (i >= 0) {
+            sum += n1[i] - '0';
+        }
+        if (j >= 0) {
+            sum += n2[j] - '0';
+        }
+        if (sum > 9) {
+            carry = 1;
+            sum -= 10;
+        } else {
+            carry = 0;
+        }
+        r[i+j+1] = sum + '0'; // store result digit in buffer
+    }
 
-	if (tens && r_index >= 0)
-	{
-		*(r + r_index) = (tens % 10) + '0';
-		return (r + r_index);
-	}
+    // add carry if there is any
+    if (carry) {
+        if (len1 + len2 >= size_r - 1) {
+            return 0;
+        }
+        r[0] = '1';
+        return &r[0];
+    }
 
-	else if (tens && r_index < 0)
-		return (0);
-
-	return (r + r_index + 1);
+    // remove leading zeros
+    int k = 0;
+    while (r[k] == '0' && k < len1 + len2 - 1) {
+        k++;
+    }
+    return &r[k];
 }
-/**
- * infinite_add - Adds two numbers.
- * @n1: The first number to be added.
- * @n2: The second number to be added.
- * @r: The buffer to store the result.
- * @size_r: The buffer size.
- *
- * Return: If r can store the sum - a pointer to the result.
- *         If r cannot store the sum - 0.
- */
-char *infinite_add(char *n1, char *n2, char *r, int size_r)
-{
-	int index, n1_len = 0, n2_len = 0;
 
-	for (index = 0; *(n1 + index); index++)
-		n1_len++;
 
-	for (index = 0; *(n2 + index); index++)
-		n2_len++;
-
-	if (size_r <= n1_len + 1 || size_r <= n2_len + 1)
-		return (0);
-
-	n1 += n1_len - 1;
-	n2 += n2_len - 1;
-	*(r + size_r) = '\0';
-
-	return (add_strings(n1, n2, r, --size_r));
-}
